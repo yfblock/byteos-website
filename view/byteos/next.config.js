2086c341598250a3a12cc025171cb88761bbb66c
yufeng <321353225@qq.com>
2023-10-12 14:11:19 +0800

    update rust.yml to fix basepath

--- view/byteos/next.config.js
+++ view/byteos/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/byteos',
     // webpack: (config, { isServer }) => {
     //     config.experiments = {
     //         asyncWebAssembly: true,
